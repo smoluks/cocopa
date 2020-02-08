d84805dba381923d3fa68b66b0dba75a165b5c74
Uli Franke <hoi@elektronikworkshop.ch>
2020-02-08 03:01:20 +0100

    0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 /* --- nothing to be found here --- */
-import {Parser} from "./Parser";
-import {Runner} from "./Runner";
-import {Result} from "./Result";
-import {ParserGcc} from "./ParserGcc";
-import {CCppProperties} from "./CCppProperties";
-import {CCppPropertiesContent} from "./CCppPropertiesContent";
-import {CCppPropertiesConfiguration} from "./CCppPropertiesConfiguration";
+import { Parser } from './Parser';
+import { Runner } from './Runner';
+import { Result } from './Result';
+import { ParserGcc } from './ParserGcc';
+import { CCppProperties } from './CCppProperties';
+import { CCppPropertiesContent } from './CCppPropertiesContent';
+import { CCppPropertiesConfiguration } from './CCppPropertiesConfiguration';
 
-export {
-    Runner,
-    Result,
-    Parser,
-    ParserGcc,
-    CCppProperties,
-    CCppPropertiesContent,
-    CCppPropertiesConfiguration,
-}
+export { Runner, Result, Parser, ParserGcc, CCppProperties, CCppPropertiesContent, CCppPropertiesConfiguration };
