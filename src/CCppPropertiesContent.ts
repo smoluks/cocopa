38ea5f81e42c10e79db181b4d57897f619429354
Uli Franke <hoi@elektronikworkshop.ch>
2020-02-22 17:03:06 +0100

    Added license information to sources and readme

--- src/CCppPropertiesContent.ts
+++ src/CCppPropertiesContent.ts
@@ -1,3 +1,21 @@
+/* CoCoPa - Compiler Command Parser, a Parser to extract include directories,
+ * defines, arguments and more from compiler command line invocations.
+ *
+ * Copyright (C) 2020 Uli Franke - Elektronik Workshop
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import {CCppPropertiesConfiguration} from "./CCppPropertiesConfiguration";
 import {removeDuplicatesFrom, arraysEqual} from "./helpers";
 
