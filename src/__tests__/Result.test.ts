bfba359bfd8bddb860bc696f47db99e5f9c5259a
Uli Franke <hoi@elektronikworkshop.ch>
2020-02-25 16:08:26 +0100

    Changed license to MIT

--- src/__tests__/Result.test.ts
+++ src/__tests__/Result.test.ts
@@ -1,3 +1,12 @@
+/* CoCoPa - Compiler Command Parser, a Parser to extract include directories,
+ * defines, arguments and more from compiler command line invocations.
+ *
+ * Copyright (C) 2020 Uli Franke - Elektronik Workshop
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ *
+ * All rights reserved.
+ */
 import {Result} from "../Result";
 
 test(`Result.normalize`, () => {
